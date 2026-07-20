13e2dd84d6c8073f70d87b5d93f4ae8009acf305
MD <maximiliandillmann144@gmail.com>
2026-07-20 17:36:38 +0200

    add cs2 match history and stats

---
+++ Models/CS2Match.cs
@@ -0,0 +1,44 @@
+﻿namespace SteamInfoPuller.Models
+{
+    public class CS2Match
+    {
+        public DateTime Date { get; set; }
+
+        public string Map { get; set; }
+
+        public string Mode { get; set; }
+
+        public bool Win { get; set; }
+
+        public int Kills { get; set; }
+
+        public int Deaths { get; set; }
+
+        public int Assists { get; set; }
+
+
+        public double KD
+        {
+            get
+            {
+                if (Deaths == 0)
+                    return Kills;
+
+                return Math.Round(
+                    (double)Kills / Deaths,
+                    2);
+            }
+        }
+
+
+        public string Result
+        {
+            get
+            {
+                return Win
+                    ? "WIN"
+                    : "LOSS";
+            }
+        }
+    }
+}
---
+++ Models/MatchStats.cs
@@ -0,0 +1,44 @@
+﻿namespace SteamInfoPuller.Models
+{
+    public class MatchStats
+    {
+        public int TotalMatches { get; set; }
+
+        public int Wins { get; set; }
+
+        public int Losses { get; set; }
+
+
+        public int TotalKills { get; set; }
+
+        public int TotalDeaths { get; set; }
+
+
+        public double AverageKD
+        {
+            get
+            {
+                if(TotalDeaths == 0)
+                    return TotalKills;
+
+                return Math.Round(
+                    (double)TotalKills / TotalDeaths,
+                    2);
+            }
+        }
+
+
+        public double WinRate
+        {
+            get
+            {
+                if(TotalMatches == 0)
+                    return 0;
+
+                return Math.Round(
+                    (double)Wins / TotalMatches * 100,
+                    1);
+            }
+        }
+    }
+}
